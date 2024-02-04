f4fe3289281ec8c836de2c547bcfb44b4e5df5ab
esraakrkmz <109651527+esraakrkmz@users.noreply.github.com>
2024-02-04 19:46:16 +0300

    Create fat32kacadetdosyavar.cs

---
+++ fat32kacadetdosyavar.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fat32_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
