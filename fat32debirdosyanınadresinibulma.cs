87dd680a07bb5b495057cb7401d4baf1037d7540
esraakrkmz <109651527+esraakrkmz@users.noreply.github.com>
2024-02-05 13:34:11 +0300

    fat32de başlangıç adresi bilinen bir dosyanın dresini bulma

---
+++ fat32debirdosyanınadresinibulma.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace dosyannınadresinibulma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eof = -1;
+            int[] fat32 = new int[100];
+            fat32[1] = 2;
+            fat32[2] = 3;
+            fat32[3] = 8;
+            fat32[8] = 52;
+            fat32[52] = eof;
+
+            int startingcluster = 1; //dosyanın başlangıç adresi
+            int pointer = startingcluster;
+            String adres = "";
+            while (pointer != eof)
+            {
+                Console.WriteLine(pointer);
+                pointer = fat32[pointer];
+                adres = pointer + "->";
+            }
+         
+
+        }
+    }
+}
