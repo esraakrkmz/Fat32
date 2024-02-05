d5a8dfbdcce489f14ff252e06d4a0d23fad6c744
esraakrkmz <109651527+esraakrkmz@users.noreply.github.com>
2024-02-05 13:47:58 +0300

    fat32 tablosunda yer alan bir  dosyanın içeriğini okuma

---
+++ fat32dedosyanıniçeriginiokuma.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace icerikokuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fat32 = new int[100]; //fat32 tablosunda clusterları tutacak int array
+            string[] hdd = new string[100]; //okuduğumuz verileri tutacağımız dizi
+            int eof = -1;
+            fat32[1] = 2;
+            fat32[2] = 3;
+            fat32[3] = 8;
+            fat32[8] = 52;
+            fat32[52] = eof;
+
+            hdd[1] = "Merhaba";
+            hdd[2] = "bu";
+            hdd[3] = "bir";
+            hdd[8] = "fat32 dizisidir";
+
+            int startingcluster = 1;
+            int pointer = startingcluster;
+            string data = "";
+
+            if (fat32[pointer] == 0)
+            {
+                Console.WriteLine("clustere is empty");
+                return;
+
+            }
+            while (pointer != eof)
+            {
+                Console.WriteLine(hdd[pointer]);
+                data += hdd[pointer];
+                pointer = fat32[pointer];
+
+
+            }
+            Console.WriteLine(data);
+        }
+    }
+}
